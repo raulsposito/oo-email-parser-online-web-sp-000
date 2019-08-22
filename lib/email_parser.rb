5cc2d30e12e89f21fb6efae5a9c4f4604e7b9354
Raul Sposito <raulsposito@gmail.com>
2019-08-22 20:25:12 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,5 +14,11 @@ class EmailAddressParser
     end
     .flatten.uniq 
   end
+  
+email_addresses = "john@doe.com, person@somewhere.org"
+parser = EmailAddressParser.new(email_addresses)
+ 
+parser.parse
+# => ["john@doe.com", "person@somewhere.org"]
 
 end
